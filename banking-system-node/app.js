94102041ed82899d73970622609bf0fd07bd2b13
ntwari-egide <ntwariegide2@gmail.com>
2022-07-02 20:06:53 +0200

    implement changes

--- banking-system-node/app.js
+++ banking-system-node/app.js
@@ -4,6 +4,8 @@ import UsersRouter from './routes/users.route.js'
 import VotedUserRouter from './routes/voted-user.route.js'
 import cors from 'cors'
 import './server.js'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 
@@ -13,7 +15,55 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json({ limit: '20kb'}))
 
+
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Voting APIs implementation documentation',
+      version: '1.0.0',
+      description:
+        'Voting app description goes here',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'innoways',
+        url: 'https://jsonplaceholder.typicode.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          in: 'header',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+    security: [{
+      bearerAuth: []
+    }]
+  };
+  
+  const options = {
+    swaggerDefinition,
+      apis: ['./routes/*.js']
+  ,
+  };
+  
+  const swaggerSpec = swaggerJSDoc(options);
+
 //  all routing to the routes folder
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use('/api/v1/accounts', AccountsRouter)
 app.use('/api/v1/users', UsersRouter)
 app.use('/api/v1/voting', VotedUserRouter )
--- banking-system-node/routes/accounts.route.js
+++ banking-system-node/routes/accounts.route.js
@@ -4,7 +4,43 @@ import { protectedMe } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: Accounts
+ *  description: accounts apis are here
+ */
 router.route('/')
+
+    /**
+     * @swagger
+     * path:
+     * /api/v1/accounts:
+     *  get:
+     *      summary: Return all accounts
+     *      description: return all candidates accounts
+     *      tags: [Accounts]
+     *      responses:
+     *          201:
+     *              description: return all candidate accounts
+     *              content:
+     *              application/json:
+     *                  schema:
+     *                      type: object
+     *                      properties:
+     *                          message:
+     *                              type: String
+     *                              example: changes
+     *                          data:
+     *                              type: array
+     *                              items:
+     *                                  type: object
+     *                                  properties:
+     *                                      id:
+        *                                      type: string
+        *                                      description: dadafs
+        *                                      example: 242341341234
+     */
     .get( protectedMe, getAllAccounts)
     .post(saveAccount)
 
