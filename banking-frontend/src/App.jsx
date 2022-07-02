fd24a3ee8bf34605e502f09c56954860828ef39f
ntwari-egide <ntwariegide2@gmail.com>
2022-07-03 00:50:56 +0200

    initialize project

---
+++ banking-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+function App() {
+
+  return (
+    <div className="App d">
+      
+    </div>
+  )
+}
+
+export default App
