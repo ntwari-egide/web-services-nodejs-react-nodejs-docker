61083f64f789cd681e65134bbf7ac29752107ec4
ntwari-egide <ntwariegide2@gmail.com>
2022-07-03 02:05:28 +0200

    implement design

--- banking-frontend/src/App.jsx
+++ banking-frontend/src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { LayoutDashboard } from './components/LayoutDashboard';
 
 function App() {
 
   return (
     <div className="App d">
-      
+      <LayoutDashboard>
+        <div className=' m-3'>
+
+            <div className=' d-flex flex-row'>
+              <div className='dark_white_bg ' style={{ width: '50%', height: '10vw'}}> </div>
+              <h2 className=' mt-4 font-weight-bold' style={{ marginLeft: '-7vw', zIndex: 1, fontWeight: 'bold'}}>You vote is secure <br></br> Your vote counts</h2>
+            </div>
+
+            <h4 className=' text-start mt-4 h6'>President of United States</h4>
+
+            <div className=' d-flex flex-row mt-4 justify-content-between'>
+                <div className=' rounded-3 dark_white_bg' style={{ width: '24vw', background: '', height: '500px'}}>
+                  <div className='votes-content'>35</div>
+                </div>
+                <div className=' rounded-3 dark_white_bg' style={{ width: '24vw', background: '', height: '500px'}}>
+                  <div className='votes-content'>35</div>
+
+                </div>
+                <div className=' rounded-3 dark_white_bg' style={{ width: '24vw', background: '', height: '500px'}}>
+                  <div className='votes-content'>35</div>
+
+                </div>
+            </div>
+
+        </div>
+      </LayoutDashboard>
     </div>
   )
 }
---
+++ banking-frontend/src/components/LayoutDashboard.jsx
@@ -0,0 +1,69 @@
+import { Layout, Menu, Image } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+import {MdOutlineDashboard} from 'react-icons/md'
+import {BsCalendarCheck} from 'react-icons/bs'
+import {FiSettings} from 'react-icons/fi'
+import { Input } from 'antd';
+import {FiSearch} from 'react-icons/fi'
+
+
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+
+const items = [
+  getItem('Home', '1', <MdOutlineDashboard />),
+  getItem('Voting', '2', <BsCalendarCheck />),
+  getItem('Settings', '3', <FiSettings />),
+];
+
+export const LayoutDashboard = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+
+    >
+      <Sider collapsible collapsed={collapsed} theme="light" className=' border-end border' onCollapse={(value) => setCollapsed(value)}>
+        <div className='container mt-5 mb-5'>
+            <Image 
+                className='rounded-circle border_red'
+                width={'80px'}
+                preview={false}
+                height={'80px'}
+                src={'https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
+            />
+
+            <h2 className=' text-center display-6 mt-4' style={{ fontSize: '16px', fontWeight: 'bold'}}>Vottingg</h2>
+        </div>
+        <Menu theme="light" defaultSelectedKeys={['2']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout" >
+        <Header
+          className="site-layout-background border-1 border-bottom"
+          style={{
+            padding: 0,
+          }}
+        >
+            <Input placeholder="Search" className=' w-50 h-60' prefix={<FiSearch />} />
+        </Header>
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          {props.children}
+        </Content>
+        
+      </Layout>
+    </Layout>
+  );
+}
